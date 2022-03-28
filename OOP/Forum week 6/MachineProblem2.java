97853fdffa8e690d12859cf58e9b5afbea4bc961
Wilbert <91464358+Rktify@users.noreply.github.com>
2022-03-28 14:42:26 +0700

    Create MachineProblem2.java

---
+++ OOP/Forum week 6/MachineProblem2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MachineProblem2{
+    public static void main(String[] args){
+        System.out.print("Enter the number of items: ");
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int array[] = new int[x];
+
+        String star = "*";
+        System.out.print("Enter the value of all items (separated by space): ");
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = input.nextInt();
+            System.out.println( i + ": " + star.repeat(array[i]) + " (" + array[i] + ")");
+        }
+    }
+}
