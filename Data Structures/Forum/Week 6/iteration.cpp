8bc40bc03279a7667786d8d7d29c33302bbea2a3
Wilbert <91464358+Rktify@users.noreply.github.com>
2022-03-28 14:14:04 +0700

    Update iteration.cpp

--- Data Structures/Forum/Week 6/iteration.cpp
+++ Data Structures/Forum/Week 6/iteration.cpp
@@ -4,15 +4,15 @@ int main() {
     int factorial, x = 1;
     cout << "Enter a number: ";
     cin >> factorial;
-
     if (factorial >= 1){
         for(int i = 1; i <= factorial; i++){
             x = x * i;
         }
-        cout << "Factorial of " << factorial << " is " << x;
     }
     else{
-        cout << "Re-enter a positive integer" << endl;
-        main();
+        x = 1;
     }
+
+    cout << "Factorial of " << factorial << " is " << x;
+
 }
